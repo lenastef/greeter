01e1163f20dabe50432e9037c06b72abdb71de81
helena stefansdottir <hel>
2023-02-07 22:37:20 +0000

    laga

---
+++ src/is/hi/hes91/hbv202g/ass3/backend.java
@@ -0,0 +1,7 @@
+package src.is.hi.hes91.hbv202g.ass3;
+
+public class backend {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/is/hi/hes91/hbv202g/ass3/frontend.java
@@ -0,0 +1,7 @@
+package src.is.hi.hes91.hbv202g.ass3;
+
+public class frontend {
+    public static void main(String[] args) {
+
+    }
+}
